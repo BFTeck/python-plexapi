bdbcd38e2449cd98e86a312196d84bcf37bd7555
BFTeck <bfteck@gmail.com>
2023-06-14 15:37:49 +0200

    Refonte pour intégrer les séries

--- download.py
+++ download.py
@@ -40,8 +40,13 @@ for ressour in account.resources():
     print("["+str(i)+"] "+ressour.name)
     i = i+1
 
-servername = input('Quel serveur ?')
-servername = account.resources()[int(servername)].name
+while True:
+    servername = input('Quel serveur ?')
+    if not servername:
+        continue
+    else:
+        servername = account.resources()[int(servername)].name
+        break
 
 plex = account.resource(servername).connect()  # returns a PlexServer instance
 i = 0
@@ -49,8 +54,16 @@ for sec in plex.library.sections():
     print("[" + str(i) + "] " + sec.title)
     i = i + 1
 
-section = input('Quelle section ?')
-section = plex.library.sections()[int(section)].title
+
+while True:
+    section = input('Quelle section ?')
+    if not section:
+        continue
+    else:
+        section = plex.library.sections()[int(section)].title
+        break
+
+
 
 movies = plex.library.section(section)
 i = 0
@@ -58,9 +71,45 @@ for video in movies.all():
     print("[" + str(i) + "] " + video.title)
     i = i + 1
 
-film = input('Quel film ?')
-film = movies.all()[int(film)].title
 
-todownload = plex.library.section(section).get(film)
+if movies.__class__.__name__ == "MovieSection":
+    
+    while True:
+        film = input('Quel film ?')
+        if not film:
+            continue
+        else:
+            itemtodownload = movies.all()[int(film)].title 
+            todownload = plex.library.section(section).get(itemtodownload)
+            break
+       
+
+
+else:
+    while True:
+        series = input('Quel serie ?')
+        if not series:
+            continue
+        else:
+            series = movies.all()[int(series)].title 
+            break
+        
+    saisons = plex.library.section(section).get(series).seasons()
+    i = 1
+    print("[0] Toutes ")
+    for saison in saisons:
+        print("[" + str(i) + "] Saison " + str(saison.index))
+        i = i + 1
+    
+    while True:
+        saisonnum = input('Quelle saison ?')
+        if not saisonnum:
+            continue
+        else:
+            todownload = plex.library.section(section).get(series).season(int(saisonnum))
+            if int(saisonnum) == 0:
+                todownload = plex.library.section(section).get(series)
+            break
+
 
 todownload.download()
